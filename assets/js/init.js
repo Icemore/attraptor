d45215d1aadb25d73a97b3fca8099d6aae1d0887
Alexey Tsvetkov <type.inference@gmail.com>
2014-11-05 16:19:46 +0400

    Minor formatting

--- assets/js/init.js
+++ assets/js/init.js
@@ -16,7 +16,7 @@ AT.init = function() {
     // PATH
     AT.parent = new THREE.Object3D();
     AT.scene.add(AT.parent);
-    AT.camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    AT.camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
     AT.parent.add(AT.camera);
 
     // when resize
@@ -24,9 +24,9 @@ AT.init = function() {
 };
 
 AT.world = function() {
-    var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xfffff , wireframe: true} );
-    AT.cylinder = new THREE.Mesh( geometry, material );
+    var geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
+    var material = new THREE.MeshBasicMaterial({ color: 0xfffff , wireframe: true});
+    AT.cylinder = new THREE.Mesh(geometry, material);
     AT.scene.add(AT.cylinder);
 };
 
