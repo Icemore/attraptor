aea1d38f0cd1a070238b1f08bb5f397440714a7b
Alexey Tsvetkov <type.inference@gmail.com>
2014-11-06 14:26:42 +0400

    Added tube

--- assets/js/world.js
+++ assets/js/world.js
@@ -34,11 +34,42 @@ AT.setUpLight = function() {
     AT.scene.add(spotLight2);
 };
 
+AT.getPath = function() {
+    return new THREE.SplineCurve3([
+        new THREE.Vector3(0, 10, -10), new THREE.Vector3(10, 0, -10), new THREE.Vector3(20, 0, 0), new THREE.Vector3(30, 0, 10), new THREE.Vector3(30, 0, 20), new THREE.Vector3(20, 0, 30), new THREE.Vector3(10, 0, 30), new THREE.Vector3(0, 0, 30), new THREE.Vector3(-10, 10, 30), new THREE.Vector3(-10, 20, 30), new THREE.Vector3(0, 30, 30), new THREE.Vector3(10, 30, 30), new THREE.Vector3(20, 30, 15), new THREE.Vector3(10, 30, 10), new THREE.Vector3(0, 30, 10), new THREE.Vector3(-10, 20, 10), new THREE.Vector3(-10, 10, 10), new THREE.Vector3(0, 0, 10), new THREE.Vector3(10, -10, 10), new THREE.Vector3(20, -15, 10), new THREE.Vector3(30, -15, 10), new THREE.Vector3(40, -15, 10), new THREE.Vector3(50, -15, 10), new THREE.Vector3(60, 0, 10), new THREE.Vector3(70, 0, 0), new THREE.Vector3(80, 0, 0), new THREE.Vector3(90, 0, 0), new THREE.Vector3(100, 0, 0)]);
+};
+
+AT.getTube = function(config) {
+    var tube = new THREE.TubeGeometry(config.path, config.segments, 2, config.radiusSegments, config.closed);
+
+    var tubeMesh = THREE.SceneUtils.createMultiMaterialObject(tube, [
+        new THREE.MeshLambertMaterial({
+            color: 0x00ff00
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0x000000,
+            opacity: 0.3,
+            wireframe: true,
+            transparent: true
+        })]);
+
+    tubeMesh.scale.set(config.scale, config.scale, config.scale);
+    return tubeMesh;
+};
+
 AT.world = function() {
     AT.attraptor = AT.getAttraptor();
     AT.scene.add(AT.attraptor);
 
-    //AT.scene.add(AT.getSpaceSphere());
+    AT.tube = AT.getTube({
+        path: AT.getPath(),
+        segments: 400,
+        radiusSegments: 3,
+        closed: true,
+        scale: 4
+    });
+
+    AT.scene.add(AT.tube);
 
     AT.setUpLight();
 };
