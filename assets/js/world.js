0b9c7de3585459f85f6751663ae5fb9d2dee5827
Alexey Tsvetkov <type.inference@gmail.com>
2014-11-06 10:38:20 +0400

    Added space background

--- assets/js/world.js
+++ assets/js/world.js
@@ -1,6 +1,40 @@
-AT.world = function() {
+AT.getCylinder = function() {
     var geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
     var material = new THREE.MeshBasicMaterial({ color: 0xfffff , wireframe: true});
-    AT.cylinder = new THREE.Mesh(geometry, material);
+    return new THREE.Mesh(geometry, material);
+};
+
+AT.getSpaceSphere = function() {
+
+    //Space background is a large sphere
+    var spacetex = THREE.ImageUtils.loadTexture("assets/img/space.jpg");
+    var spacesphereGeo = new THREE.SphereGeometry(40, 40, 40);
+    var spacesphereMat = new THREE.MeshPhongMaterial();
+    spacesphereMat.map = spacetex;
+
+    //spacesphere needs to be double sided as the camera is within the spacesphere
+    var spacesphere = new THREE.Mesh(spacesphereGeo, spacesphereMat);
+    spacesphere.material.side = THREE.DoubleSide;
+    spacesphere.material.map.wrapS = THREE.RepeatWrapping;
+    spacesphere.material.map.wrapT = THREE.RepeatWrapping;
+    spacesphere.material.map.repeat.set(5, 3);
+
+    return spacesphere;
+};
+
+AT.world = function() {
+    AT.cylinder = AT.getCylinder();
     AT.scene.add(AT.cylinder);
+
+    AT.scene.add(AT.getSpaceSphere());
+
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-40, 60, -10);
+    spotLight.intensity = 2;
+    AT.scene.add(spotLight);
+
+    var spotLight2 = new THREE.SpotLight(0x5192e9);
+    spotLight2.position.set(40, -60, 30);
+    spotLight2.intensity = 1.5;
+    AT.scene.add(spotLight2);
 };
