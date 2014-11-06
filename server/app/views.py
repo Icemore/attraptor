3410e791e2728f23fc1f74ac07e7addd3e34aedf
Yury Fettser <fettsery@gmail.com>
2014-11-06 14:49:09 +0300

    proof-of-concept version

--- server/app/views.py
+++ server/app/views.py
@@ -1,16 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app, db
 from models import User, ROLE_USER, ROLE_ADMIN
+from forms import UsernameForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Petr' }
     posts = list()
- 
-#    newuser = User(nickname = 'Chubakka', darkside=1,result=101, role = ROLE_USER)
-#    db.session.add(newuser)
-#    db.session.commit()
     users = User.query.order_by(User.result.desc()).all()
     for u in users:
         if u.darkside == 1:
@@ -19,5 +15,17 @@ def index():
             posts.append({'author': {'nickname':u.nickname}, 'result': u.result})
     return render_template("index.html",
         title = 'Home',
-        user = user,
         posts = posts)
+@app.route('/')
+@app.route('/gameover', methods = ['GET', 'POST'])
+def gameover():
+    form = UsernameForm()
+    if form.validate_on_submit():
+        newuser = User(nickname = form.username.data, darkside=0,result=404, role = ROLE_USER)
+        db.session.add(newuser)
+        db.session.commit()	    
+        flash('Added username ' + form.username.data + ' with some result')
+        return redirect('/index')
+    return render_template("gameover.html",
+		title = 'Gameover',
+        form = form)
