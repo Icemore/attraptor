beb1462b3e6f22f1b69e0afa298d202f8cddc277
Alexey Tsvetkov <type.inference@gmail.com>
2014-11-06 12:29:48 +0400

    Added update function

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,5 +1,12 @@
 AT.render = function() {
     requestAnimationFrame(AT.render);
+
+    AT.lastMsec = AT.nowMsec;
+    AT.nowMsec = window.performance.now();
+    var delta = AT.nowMsec - AT.lastMsec;
+
+    AT.update(delta);
+
     AT.cylinder.rotation.z += 0.01;
     AT.cylinder.rotation.y += 0.1;
     AT.renderer.render(AT.scene, AT.camera);
---
+++ assets/js/update.js
@@ -0,0 +1,3 @@
+AT.update = function(delta) {
+
+};
