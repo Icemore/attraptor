e42c7f752f56fbdff7e4ebdbd696904103471df2
nunberty <kramar.alina@gmail.com>
2014-11-06 14:58:32 +0000

    add music upload to main page

--- assets/js/music/music.js
+++ assets/js/music/music.js
@@ -1,10 +1,10 @@
 AT.Music = {};
 
 AT.Music.audio = new Audio();
-AT.Music.audioSrc = "sample.mp3";
-AT.Music.audioCtx = null;
+AT.Music.source = null;
+AT.Music.context = null;
 AT.Music.analyser = null;
-AT.Music.audioBuf = null;
+AT.Music.buffer = null;
 AT.Music.setup = false;
 AT.Music.N = 2048;
 AT.freq = 0;
@@ -12,8 +12,11 @@ AT.freq = 0;
 AT.Music.init = function() {
     console.log("init music");
     try {
-        this.audioCtx = new webkitAudioContext();
-        this.loadFile();
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+        } else {
+            alert('The File APIs are not fully supported in this browser.');
+        }
+        this.context = new webkitAudioContext();
         var loopy_loop = function() {
             AT.Music.onTick();
             requestAnimationFrame(loopy_loop)
@@ -33,7 +36,11 @@ AT.Music.onTick = function() {
     var data = new Uint8Array(1);
     AT.Music.analyser.getByteFrequencyData(data);
     AT.freq = data[0];
-    console.log(data[0]);
+};
+
+AT.Music.uploadAudion = function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+    console.log(files);
 };
 
 AT.Music.loadFile = function() {
@@ -41,28 +48,53 @@ AT.Music.loadFile = function() {
     req.open("GET",AT.Music.audioSrc,true);
     req.responseType = "arraybuffer";
     req.onload = function() {
-        AT.Music.audioCtx.decodeAudioData(req.response, function(buffer) {
-            AT.Music.audioBuf = buffer;
+        AT.Music.context.decodeAudioData(req.response, function(buffer) {
+            AT.Music.buffer = buffer;
             AT.Music.play();
         });
     };
     req.send();
 };
 
-AT.Music.play = function() {
-    var src = this.audioCtx.createBufferSource();
-    src.buffer = this.audioBuf;
+AT.Music.play = function(arraybuffer) {
+    AT.Music.context.decodeAudioData(arraybuffer, function (buf) {
+        AT.Music.source = AT.Music.context.createBufferSource();
+        AT.Music.source.connect(AT.Music.context.destination);
+        AT.Music.source.buffer = buf;
+        AT.Music.source.start(0);
+    });
+};
 
-    this.analyser = this.audioCtx.createAnalyser();
-    this.analyser.fftSize = this.N;
+AT.Music.loadAndPlay = function(file) {
+    var freader = new FileReader();
 
-    src.connect(this.analyser);
-    this.analyser.connect(this.audioCtx.destination);
-    console.log(src.noteOn);
-    src.start();
-    this.setup = true;
+    freader.onload = function (e) {
+        console.log(e);
+        AT.Music.play(e.target.result);
+    };
+    freader.readAsArrayBuffer(file);
+};
+
+function handleFileSelect(evt) {
+    var files = evt.target.files;
+
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+            f.size, ' bytes, last modified: ',
+            f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+            '</li>');
+    }
+
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    if (files[0].type.indexOf("audio") == 0) {
+        AT.Music.loadAndPlay(files[0]);
+    } else {
+        alert(files[0].name + " is not a audio=(")
+    }
 };
 
 $(function() {
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
     AT.Music.init();
 });
