480b3e6865979895ff773b65cdc250cd40f8ecb2
Anton Afanasev <okajsler@yandex.ru>
2014-11-07 15:09:18 +0300

    better points counting

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,9 +6,9 @@ AT.game = {
     minHealth: 0,
     maxHealth: 10,
 
-    scoreGain: {'asteroids': 100, 'good': 100, 'bad': -100},
+    scoreGain: {'asteroids': 0, 'good': 100, 'bad': -100},
     karmaGain: {'asteroids':  0, 'good': 1, 'bad': -1},
-    healthGain: {'asteroids': 0, 'good': 0, 'bad': 0}
+    healthGain: {'asteroids': -1, 'good': 0, 'bad': 0}
 };
 
 AT.game.init = function() {
@@ -29,13 +29,22 @@ AT.game.getAttraptorModel = function() {
     return AT.attraptor.models[[this.curKarma, this.curHealth]];
 };
 
+AT.game.die = function() {
+    console.log("dead");
+    AT.game.curHealth = 0;
+};
+
 AT.game.processCollision = function(tag, obj) {
     this.curHealth += this.healthGain[tag];
     this.curHealth = Math.min(this.curHealth, this.maxHealth);
+    if(this.curHealth < 0) {
+        AT.game.die();
+    }
 
     this.curPoints += this.scoreGain[tag];
 
-    this.curKarma += this.karmaGain[tag];
+    //this.curKarma += this.karmaGain[tag];
+    this.curKarma = (this.curPoints / 1000) | 0;
     this.curKarma = Math.min(this.curKarma, this.maxKarma);
     this.curKarma = Math.max(this.curKarma, this.minKarma);
 
